e4c5c4031b46fde1cee9143aab269999e3be9032
Jahid <jahid4k@gmail.com>
2022-03-11 02:20:27 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', )
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
